56b71419c8067c75eef7555a892abdd59abdf1f6
GalaxyGamerYT <OliWootton@Outlook.com>
2022-11-19 21:02:07 +0000

    magic

--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ from debug import debug
 from support import *
 from random import choice
 from weapon import Weapon
+from ui import UI
 
 class Level:
     def __init__(self):
@@ -22,6 +23,9 @@ class Level:
 
         # sprite setup
         self.create_map()
+
+        # user interface
+        self.ui = UI()
 
     def create_map(self):
         layouts = {
@@ -51,10 +55,21 @@ class Level:
                             # create a gobject tile
                             surf = graphics['objects'][int(col)]
                             Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
-        self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites,self.create_attack,self.destroy_attack)
+        self.player = Player(
+            (2000,1430),
+            [self.visible_sprites],
+            self.obstacle_sprites,
+            self.create_attack,
+            self.destroy_attack,
+            self.create_magic)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player,[self.visible_sprites])
+
+    def create_magic(self,style,strength,cost):
+        print(style)
+        print(strength)
+        print(cost)
 
     def destroy_attack(self):
         if self.current_attack:
@@ -65,6 +80,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
--- code/settings.py
+++ code/settings.py
@@ -4,6 +4,25 @@ HEIGHT   = 720
 FPS      = 60
 TILESIZE = 64
 
+# ui
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = 'graphics/font/joystix.ttf'
+UI_FONT_SIZE = 18
+
+# general colours
+WATER_COLOUR = '#71ddee'
+UI_BG_COLOUR = '#222222'
+UI_BORDER_COLOUR = '#111111'
+TEXT_COLOUR = '#EEEEEE'
+
+# ui colours
+HEALTH_COLOUR = 'red'
+ENERGY_COLOUR = 'blue'
+UI_BORDER_COLOUR_ACTIVE = 'gold'
+
 # weapons 
 weapon_data = {
     'sword': {'cooldown': 100, 'damage': 15,'graphic':'graphics/weapons/sword/full.png'},
@@ -11,3 +30,8 @@ weapon_data = {
     'axe': {'cooldown': 300, 'damage': 20, 'graphic':'graphics/weapons/axe/full.png'},
     'rapier': {'cooldown': 50, 'damage': 8, 'graphic':'graphics/weapons/rapier/full.png'},
     'sai': {'cooldown': 80, 'damage': 10, 'graphic':'graphics/weapons/sai/full.png'}}
+
+# magic
+magic_data = {
+    'flame': {'strength': 5,'cost': 20,'graphic':'graphics/particles/flame/fire.png'},
+    'heal' : {'strength': 20,'cost': 10,'graphic':'graphics/particles/heal/heal.png'}}
