8e0c625981ea60e68a0912ff1220d58cba0e0e65
GalaxyGamerYT <OliWootton@Outlook.com>
2022-11-25 22:08:26 +0000

    player-enemy interaction

--- code/entity.py
+++ code/entity.py
@@ -1,5 +1,5 @@
-from flask import g
 import pygame
+from math import sin
 
 class Entity(pygame.sprite.Sprite):
     def __init__(self,groups):
@@ -7,7 +7,6 @@ class Entity(pygame.sprite.Sprite):
         self.frame_index = 0
         self.animation_speed = 0.15
         self.direction = pygame.math.Vector2()
-
 
     def move(self,speed):
         if self.direction.magnitude() != 0:
@@ -36,3 +35,9 @@ class Entity(pygame.sprite.Sprite):
                     if self.direction.y < 0: # moving up
                         self.hitbox.top = sprite.hitbox.bottom
 
+    def wave_value(self):
+        value = sin(pygame.time.get_ticks())
+        if value >= 0:
+            return 255
+        else:
+            return 0
--- code/level.py
+++ code/level.py
@@ -21,6 +21,8 @@ class Level:
         
         # attack sprites
         self.current_attack = None
+        self.attack_sprites = pygame.sprite.Group()
+        self.attackable_sprites = pygame.sprite.Group()
 
         # sprite setup
         self.create_map()
@@ -52,7 +54,11 @@ class Level:
                         if style == 'grass':
                             # create a grass tile
                             random_grass_image = choice(graphics['grass'])
-                            Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass',random_grass_image)
+                            Tile(
+                                (x,y),
+                                [self.visible_sprites,self.obstacle_sprites,self.attackable_sprites],
+                                'grass',
+                                random_grass_image)
                         
                         if style == 'object':
                             # create a gobject tile
@@ -73,10 +79,15 @@ class Level:
                                 elif col == '391': monster_name = 'spirit'
                                 elif col == '392': monster_name = 'raccoon'
                                 else: monster_name = 'squid'
-                                Enemy(monster_name,(x,y),[self.visible_sprites],self.obstacle_sprites)
+                                Enemy(
+                                    monster_name,
+                                    (x,y),
+                                    [self.visible_sprites,self.attackable_sprites],
+                                    self.obstacle_sprites,
+                                    self.damage_player)
 
     def create_attack(self):
-        self.current_attack = Weapon(self.player,[self.visible_sprites])
+        self.current_attack = Weapon(self.player,[self.visible_sprites,self.attack_sprites])
 
     def create_magic(self,style,strength,cost):
         print(style)
@@ -88,11 +99,30 @@ class Level:
             self.current_attack.kill()
         self.current_attack = None
 
+    def player_attack_logic(self):
+        if self.attack_sprites:
+            for attack_sprite in self.attack_sprites:
+                collision_sprites = pygame.sprite.spritecollide(attack_sprite,self.attackable_sprites,False)
+                if collision_sprites:
+                    for target_sprite in collision_sprites:
+                        if target_sprite.sprite_type == 'grass':
+                            target_sprite.kill()
+                        else:
+                            target_sprite.get_damage(self.player,attack_sprite.sprite_type)
+
+    def damage_player(self,amount,attack_type):
+        if self.player.vulnerable:
+            self.player.health -= amount
+            self.player.vulnerable = False
+            self.player.hurt_time = pygame.time.get_ticks()
+            # spawn particles
+
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         self.visible_sprites.enemy_update(self.player)
+        self.player_attack_logic()
         self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
